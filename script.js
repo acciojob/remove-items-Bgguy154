62f283444a0f32dbeec927993314ccd7faa95115
Bgguy154 <144231038+Bgguy154@users.noreply.github.com>
2025-11-10 17:44:45 +0530

    created a submission

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ let option=document.getElementById('colorSelect');
 let button=document.querySelector('input[type="button"]');
 
 button.addEventListener('click',()=>{
-	let selected=option.selected;
+	let selected=option.selectedIndex;
 	if(selected!=-1){
 		option.remove(selected);
 	}
