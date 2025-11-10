9f23f681a43632722e9e90471c28e02abd019d8b
Bgguy154 <144231038+Bgguy154@users.noreply.github.com>
2025-11-10 17:41:29 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
 //your JS code here. If required.
+let option=document.getElementById('colorSelect');
+let button=document.querySelector('input[type="button"]');
+
+button.addEventListener('click',()=>{
+	let selected=option.selected;
+	if(selected!=-1){
+		option.remove(selected);
+	}
+})
